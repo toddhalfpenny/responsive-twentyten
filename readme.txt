915468b2f766a8a8f532146dec2b6dcdf588b3af
toddhalfpenny <toddhalfpenny@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-08 10:03:29 +0000

    First drop... hope y'all like it
    git-svn-id: http://plugins.svn.wordpress.org/responsive-twentyten/trunk@342319 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== Responsive TwentyTen ===
+Contributors: toddhalfpenny
+Donate link: http://gingerbreaddesign.co.uk/wordpress/plugins/plugins.php
+Tags: css, twentyten, responsive
+Requires at least: 2.8
+Tested up to: 3.1
+Stable tag: 0.0.1
+
+Makes your TwentyTen themed site have a responsive and fluid layout
+
+
+== Description ==
+
+Makes your TwentyTen themed site have a responsive and fluid layout. Making it ideal for for viewing across a whole range of devices and screen sizes (i.e iPhones, Android, iPads).
+Uses CSS and media queries to enable the fluidity and responsiveness.
+
+
+
+== Installation ==
+
+1. Install the plugin from within the Dashboard or upload the directory `responsive-twentyten` and all its contents to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Voila!
+
+
+== Frequently Asked Questions ==
+
+= Why is this not a child theme = 
+
+The WordPress.org theme repository doesn't support the submission of child themes yet. This is meant to be on it's way some time soon... but in the meantime a similar result can be gotten with this plugin.
+
+
+== Screenshots ==
+
+1. Normal browser
+
+2. Narrow Browser
+
+3. iPhone Browser
+
+
+== Changelog ==
+
+= 0.0.1 = 
+
+First Release.
+
+1st release - only supports one defined in-post/page widget area
---
+++ responsive-twentyten.php
@@ -0,0 +1,39 @@
+<?php
+
+/*  Copyright 2010  TODD HALFPENNY  (email : todd@gingerbreaddesign.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+Plugin Name: Responsive TwentyTen
+Plugin URI: http://gingerbreaddesign.co.uk/wordpress/responsive-twentyten/
+Description: Makes your TwentyTen themed site have a responsive and fluid layout. Making it ideal for for viewing across a whole range of devices and screen sizes (i.e iPhones, Android, iPads)
+Author: Todd Halfpenny
+Version: 0.0.1
+Author URI: http://gingerbreaddesign.co.uk/todd
+*/
+
+
+function rtt_add_css_and_js() {
+  echo '<link rel="stylesheet" href="' . get_bloginfo('wpurl') .'/wp-content/plugins/responsive-twentyten/css/style.css" media="screen and (min-device-width: 481px)" type="text/css" />
+    <link type="text/css" rel="stylesheet" media="only screen and (max-device-width: 480px)" href="' . get_bloginfo('wpurl') .'/wp-content/plugins/responsive-twentyten/css/phone_style.css" />
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, 
+ maximum-scale=1.0">';
+}
+
+add_action('wp_head', 'rtt_add_css_and_js');
+
+?>
